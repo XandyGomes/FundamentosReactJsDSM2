c3ded85877d281fd04bc9653871163215dac8c27
XandyGomes <x3.computadores@hotmail.com>
2022-10-27 22:19:49 -0300

    Aula.27.10.2022

---
+++ src/componentes/Componente1.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function funct1() {
+    return <h1>Nosso primeiro componente</h1>
+}
+
+export default funct1
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Componente1 from "./componentes/Componente1";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const element = document.getElementById('root')
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+    <div>
+        <Componente1/>
+    </div>
+, element
+)
